0fa3c33900f1921990682c0fbc0e3275927763c2
RaptorboxN <vanderburgnick@gmail.com>
2017-06-22 21:01:59 +0200

    Rechtzetting
    recht gezet zodat kees jan kan starten met stuff

--- resources/views/layouts/profiel.blade.php
+++ resources/views/layouts/profiel.blade.php
@@ -1,38 +1,47 @@
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<div class="container">
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<div class="container profile">
     <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-6">
-            <div class="well well-sm">
-                <div class="row">
-                    <div class="col-sm-6 col-md-4">
-                        <img src="http://placehold.it/500x500" class="img-rounded img-responsive" draggable="false" />
-                        <div style="padding-top:15px;"></div>
-                        <span class="label label-warning">Global Rank: #35122</span>
+        <div class="span12">
+            <div class="well well-small clearfix">
+                <div class="row-fluid">
+                    <div class="span2">
+                        <img src="http://www.gravatar.com/avatar/f6112e781842d6a2b4636b35451401ff?s=125" class="img-polaroid"/>
                     </div>
-                    <div class="col-sm-6 col-md-8">
-                        <span class="label label-warning pull-right">Level: 1</span>
-                        <h4>Casper Thomsen</h4>
-                        <small>
-                            <cite title="San Francisco, USA">Harboøre, Denmark <i class="glyphicon glyphicon-map-marker"></i></cite>
-                        </small>
-                        <p>
-                            <i class="glyphicon glyphicon-envelope"></i>user@mail.com
-                            <br />
-                            <i class="glyphicon glyphicon-globe"></i><a href="http://www.jquery2dotnet.com">www.jquery2dotnet.com</a>
-                            <br />
-                            <i class="glyphicon glyphicon-gift"></i>March 12, 1997
-                        </p>
-                        <hr>
-                        <div class="row nav">
-                            <div class="col-md-8" style="margin: 0px;padding-top: 10px;">
-                                <div class="col-md-4 friendly"><i class="fa fa-smile-o fa-lg"></i> 16</div>
-                                <div class="col-md-4 heart"><i class="fa fa-heart-o fa-lg"></i> 14</div>
-                                <div class="col-md-4 like"><i class="fa fa-thumbs-o-up fa-lg"></i> 26</div>
-                            </div>
-                        </div>
+                    <div class="span4">
+                        <h2>Jonnie Spratley</h2>
+                        <ul class="unstyled">
+                            <li><i class="icon-phone"></i> 916-241-3613</li>
+                            <li><i class="icon-envelope"></i> jonniespratley@me.com</li>
+                            <li><i class="icon-globe"></i> http://jonniespratley.me</li>
+                        </ul>
                     </div>
+                    <div class="span6">
+                        <ul class="inline stats">
+                            <li>
+                                <span>275</span>
+                                Friends
+                            </li>
+                            <li>
+                                <span>354</span>
+                                Followers
+                            </li>
+                            <li>
+                                <span>186</span>
+                                Photos
+                            </li>
+                        </ul>
+                        <div><!--/span6-->
+                        </div><!--/row-->
+                    </div>
+                    <!--Body content-->
                 </div>
             </div>
+
+            <div class="well clearfix">
+
+
+
+                <img class="pull-right" src="http://kcdn3.klout.com/static/images/developers/dev-assets/powered-by-klout.png" width="150px"/>
+            </div>
         </div>
-    </div>
-</div>
